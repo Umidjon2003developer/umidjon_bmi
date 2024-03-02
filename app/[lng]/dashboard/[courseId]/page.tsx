505d49acc2f74240ad299c51cdafddb027329a23
samarbadriddin0v <info@sammi.ac>
2024-03-02 13:25:18 +0200

    #24. Video player & Curriculum

--- app/[lng]/dashboard/[courseId]/page.tsx
+++ app/[lng]/dashboard/[courseId]/page.tsx
@@ -1,5 +1,15 @@
-function Page() {
-	return <div>Page</div>
+import { getLastLesson } from '@/actions/lesson.action'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+interface Props {
+	params: { courseId: string; lng: string }
+}
+async function Page({ params: { courseId, lng } }: Props) {
+	const { userId } = auth()
+	const { lessonId, sectionId } = await getLastLesson(userId!, courseId)
+
+	return redirect(`/${lng}/dashboard/${courseId}/${lessonId}?s=${sectionId}`)
 }
 
 export default Page
--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -42,7 +42,7 @@ function RootLayout({ children, params: { lng } }: Props) {
 		<ClerkProvider localization={local}>
 			<html lang={lng} dir={dir(lng)} suppressHydrationWarning>
 				<body
-					className={`${roboto.variable} ${spaceGrotesk.variable} overflow-x-hidden`}
+					className={`${roboto.variable} ${spaceGrotesk.variable} custom-scrollbar overflow-x-hidden`}
 					suppressHydrationWarning
 				>
 					<ThemeProvider
@@ -52,7 +52,7 @@ function RootLayout({ children, params: { lng } }: Props) {
 						disableTransitionOnChange
 					>
 						<Toaster position='top-center' />
-						{children}
+						<div>{children}</div>
 					</ThemeProvider>
 				</body>
 			</html>
