80014922d424b02e960233fada5a30ac5f764b89
samarbadriddin0v <info@sammi.ac>
2024-03-16 08:45:56 +0200

    #40. Notification

--- app/[lng]/(root)/become-instructor/_components/instructor-form.tsx
+++ app/[lng]/(root)/become-instructor/_components/instructor-form.tsx
@@ -19,6 +19,7 @@ import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import useTranslate from '@/hooks/use-translate'
+import { sendNotification } from '@/actions/notification.action'
 
 function InstructorForm() {
 	const [progress, setProgress] = useState(33)
@@ -85,12 +86,16 @@ function InstructorForm() {
 		const onSubmit = async (values: z.infer<typeof bioSchema>) => {
 			setLoading(true)
 
-			return updateUser({
+			const upd = updateUser({
 				clerkId: userId!,
 				updatedData: { ...values, approvedInstructor: true },
 			})
 				.then(() => setStep(4))
 				.finally(() => setLoading(false))
+
+			const not = sendNotification(userId!, 'messageInstructorApproved')
+
+			return Promise.all([upd, not])
 		}
 
 		return (
